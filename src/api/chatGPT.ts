813276f1786f9674c608e3fe4c119f28f82500f4
차현우 <chahyunwoo@chahyeon-uui-MacBookPro.local>
2023-03-21 19:43:18 +0900

    todo: refactoring

--- src/api/chatGPT.ts
+++ src/api/chatGPT.ts
@@ -1,33 +1,32 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
 const openaiApi = axios.create({
-  baseURL: "https://api.openai.com/v1/engines/text-davinci-003/completions",
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-    "Content-Type": "application/json",
-  },
+	baseURL: 'https://api.openai.com/v1/engines/text-davinci-003/completions',
+	headers: {
+		'Authorization': `Bearer ${API_KEY}`,
+		'Content-Type': 'application/json',
+	},
 });
 
 export const fetchChatResponse = async (prompt: string) => {
-  try {
-    const response = await openaiApi.post("", {
-      prompt: prompt,
-      temperature: 0.9,
-      max_tokens: 300,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6,
-      stop: ["Human:", "AI:"],
-    });
+	try {
+		const response = await openaiApi.post('', {
+			prompt: prompt,
+			temperature: 0.9,
+			max_tokens: 300,
+			top_p: 1,
+			frequency_penalty: 0,
+			presence_penalty: 0.6,
+			stop: ['Human:', 'AI:'],
+		});
 
-    const aiResponse = response.data.choices[0].text.trim();
-    const cleanedAiResponse = aiResponse.replace(/^:\s*/, "");
+		const aiResponse = response.data.choices[0].text.trim();
+		const cleanedAiResponse = aiResponse.replace(/^:\s*/, '');
 
-    return cleanedAiResponse;
-  } catch (error) {
-    console.error("Error fetching chat response:", error);
-    throw error;
-  }
+		return cleanedAiResponse;
+	} catch (error) {
+		console.error('Error fetching chat response:', error);
+	}
 };
--- src/styles/components/InfoStyle.ts
+++ src/styles/components/InfoStyle.ts
@@ -1,97 +1,98 @@
-import styled from "styled-components";
-import tw from "twin.macro";
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 export const InfoBox = styled.article`
-  ${tw`
+	${tw`
       text-white
       p-5
       flex
       flex-wrap
       relative
   `}
-  border: 1px solid rgba(255, 255, 255, 0.5);
-  width: 100%;
-  border-radius: 5px;
-  height: 25vh;
+	border: 1px solid rgba(255, 255, 255, 0.5);
+	width: 100%;
+	border-radius: 5px;
+	height: 25vh;
 
-  .spinner {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
+	.spinner {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
 
-  @media screen and (max-width: 1280px) {
-    height: auto;
-  }
+	@media screen and (max-width: 1280px) {
+		height: auto;
+	}
 `;
 
 export const LeftBox = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+	width: 50%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 
-  p {
-    width: 100%;
-    text-align: center;
-    margin-bottom: 20px;
-  }
+	p {
+		width: 100%;
+		text-align: center;
+		margin-bottom: 20px;
+	}
 
-  div {
-    width: 15%;
-    height: auto;
-    margin: 0 auto;
-  }
+	div {
+		width: 15%;
+		height: auto;
+		margin: 0 auto;
+	}
 
-  svg {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+	svg {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
 
-  @media screen and (max-width: 1280px) {
-    width: 100%;
+	@media screen and (max-width: 1280px) {
+		width: 100%;
 
-    p {
-      font-size: 13px;
-    }
+		p {
+			font-size: 13px;
+		}
 
-    div {
-      width: 80px;
-      margin-bottom: 20px;
-    }
-  }
+		div {
+			width: 80px;
+			margin-bottom: 20px;
+		}
+	}
 `;
 
 export const RightBox = styled.ul`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  padding-left: 100px;
+	width: 50%;
+	display: flex;
+	flex-direction: column;
+	padding-left: 100px;
+	justify-content: center;
 
-  li {
-    width: 100%;
-    font-size: 14px;
-    letter-spacing: 1px;
-    margin-bottom: 10px;
+	li {
+		width: 100%;
+		font-size: 14px;
+		letter-spacing: 1px;
+		margin-bottom: 10px;
 
-    &:last-of-type {
-      margin-bottom: 0;
-    }
-  }
+		&:last-of-type {
+			margin-bottom: 0;
+		}
+	}
 
-  @media screen and (max-width: 1280px) {
-    width: 100%;
-    padding-left: 0;
+	@media screen and (max-width: 1280px) {
+		width: 100%;
+		padding-left: 0;
 
-    li {
-      margin-bottom: 10px;
-      text-align: center;
+		li {
+			margin-bottom: 10px;
+			text-align: center;
 
-      &:last-of-type {
-        margin-bottom: 0;
-      }
-    }
-  }
+			&:last-of-type {
+				margin-bottom: 0;
+			}
+		}
+	}
 `;
